8bd99e8784a958e19665cc1c51498edf2027408f
JeppeKjeldal <jeppekjeldal95@gmail.com>
2024-02-06 09:29:21 +0100

    VS project

---
+++ ThreadProject/ThreadProject/Tree.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadProject
+{
+    internal class Tree : Resource
+    {
+        private Texture2D[] trees;
+        private Random rnd = new Random();
+
+
+        public Tree()
+        {
+            position.X = rnd.Next(10, 200);
+            position.Y = rnd.Next(600, 800);
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            trees = new Texture2D[2];
+            trees[0] = content.Load<Texture2D>("tree-light-green-isaiah658");
+            trees[1] = content.Load<Texture2D>("tree-dark-green-isaiah658");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw(trees[0], new Vector2(300, 700), Color.White);
+            //spriteBatch.Draw(trees[1], new Vector2(400, 700), Color.White);
+
+            for (int i = 0; i < 10; i++)
+            {
+                spriteBatch.Draw(trees[0], position, Color.White);
+                spriteBatch.Draw(trees[1], position, Color.White);
+                //position.X = rnd.Next(10, 200);
+                //position.Y = rnd.Next(600, 800);
+            }
+        }
+    }
+}
---
+++ ThreadProject/ThreadProject/Worker.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadProject
+{
+    internal class Worker : GameObject
+    {
+        public override void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>("dwarf-male-base");
+            position = new Vector2(875 , 575);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, Color.White);
+        }
+    }
+}
