555cae0f84e4776489c47fc5872878d5627b3313
axpterrax1 <axp_terrax@hotmail.com>
2024-02-07 12:49:47 +0100

    gold lock and wood lock

--- ThreadProject/ThreadProject/UI Manager.cs
+++ ThreadProject/ThreadProject/UI Manager.cs
@@ -13,67 +13,24 @@ namespace ThreadProject
 {
     internal class UI_Manager
     {
-        private int goldAmount = 50;
-        private int woodAmount = 50;
+        static public int goldAmount = 50;
+        static public int woodAmount = 50;
         private int workerPrice;
         private int warriorPrice;
         private Texture2D goldSprite;
         private Texture2D woodSprite;
         private SpriteFont uiFont;
         private Vector2 woodScale;
-        Thread goldThread;
-        Thread woodThread;
 
         public UI_Manager()
         {
-            goldThread = new Thread(() => ThreadGold());
-            goldThread.IsBackground = true;
-            woodThread = new Thread(() => ThreadWood());
-            woodThread.IsBackground = true;
             woodScale = new Vector2(0.1f);
         }
-
-        private void ThreadGold()
-        {
-            while (true)
-            {
-                KeyboardState keyState = Keyboard.GetState();
-
-                if (keyState.IsKeyDown(Keys.Q))
-                {
-                    goldAmount += 10;
-                }
-                else if (keyState.IsKeyDown(Keys.B))
-                {
-                    goldAmount -= 10;
-                }
-            }
-        }
-
-        private void ThreadWood()
-        {
-            while (true)
-            {
-                KeyboardState keyState = Keyboard.GetState();
-
-                if (keyState.IsKeyDown(Keys.W))
-                {
-                    woodAmount += 10;
-                }
-                else if (keyState.IsKeyDown(Keys.L))
-                {
-                    woodAmount -= 10;
-                }
-            }
-        }
-
-        
 
         public void LoadContent(ContentManager content)
         {
             goldSprite = content.Load<Texture2D>("Gold");
             woodSprite = content.Load<Texture2D>("Wood");
-
             uiFont = content.Load<SpriteFont>("File");
         }
 
@@ -83,12 +40,6 @@ namespace ThreadProject
             spriteBatch.DrawString(uiFont, "" + goldAmount, new Vector2(70, 880), Color.White);
             spriteBatch.Draw(woodSprite, new Vector2(110, 850), null, Color.White, 0, new Vector2(0, 0), woodScale, SpriteEffects.None, 1f);
             spriteBatch.DrawString(uiFont, "" + woodAmount, new Vector2(150, 880), Color.White);
-        }
-        
-        public void Start()
-        {
-            goldThread.Start();
-            woodThread.Start();
         }
     }
 }
