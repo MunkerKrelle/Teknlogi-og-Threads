bc770a44a84e4d95006928a1968526788b9bfe0e
O-Verno <oliver.nvj@icloud.com>
2024-02-09 09:26:51 +0100

    up

--- ThreadProject/ThreadProject/GameWorld.cs
+++ ThreadProject/ThreadProject/GameWorld.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Web;
 
 namespace ThreadProject
 { 
